33fda7136f51561ee68655cb57cc5efafe6588b6
Ben Fioresi <bmfioresi@crimson.ua.edu>
2024-02-08 14:14:33 -0600

    Delete drive-test directory
    This directory isn't necessary anymore

--- drive-test/xmltest.js
+++
@@ -1,8 +0,0 @@
-async function searchFile() {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', '')
-    xhr.onreadystatechange = function (e) {
-        console.log(xhr.response);
-    };
-    xhr.send
-}

8c57e8572f0e5d0e46135d1dc999e024ab998bb9
hbrousseau <brousseau.hannah8@gmail.com>
2024-02-12 21:56:41 -0600

    Same as the previous commit

---
+++ client/src/pages/desktop-sign-in.js
@@ -0,0 +1,13 @@
+import LeftSide8Column from "../components/login-components/left-side8-column";
+import "./desktop-sign-in.css";
+
+const DesktopSignIn = () => {
+  return (
+    <div className="desktop-sign-in">
+      <LeftSide8Column />
+      <img className="art-icon" loading="eager" alt="" src="/art@2x.png" />
+    </div>
+  );
+};
+
+export default DesktopSignIn;

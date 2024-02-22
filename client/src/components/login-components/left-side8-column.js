88fdec9539193ffd74f8ef76b755793e31298afa
Liam-Tucker3 <letucker2003@gmail.com>
2024-02-22 02:05:57 +0000

    Modifying login page to redirect to profile page

--- client/src/components/login-components/left-side8-column.js
+++ client/src/components/login-components/left-side8-column.js
@@ -1,11 +1,19 @@
-import { useState, useCallback } from "react";
+import React, { useState, useEffect, useCallback } from "react";
+import ReactDOM from "react-dom/client";
+import { useNavigate, Navigate } from "react-router-dom";
+import axios, { formToJSON } from 'axios';
 import Input from "./input";
-import { useNavigate } from "react-router-dom";
 import "./left-side8-column.css";
 
 const LeftSide8Column = () => {
-  const [placeholderText1Value, setPlaceholderText1Value] = useState("");
   const navigate = useNavigate();
+
+  // Used for facilitating login
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const [loggedIn, setLoggedIn] = useState("FALSE");
 
   const onForgotPasswordTextClick = useCallback(() => {
     navigate("/forgotpassword");
@@ -18,6 +26,35 @@ const LeftSide8Column = () => {
   const onDontYouHaveClick = useCallback(() => {
     navigate("/signup");
   }, [navigate]);
+
+  const handleLoginChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevState) => ({ ...prevState, [name]: value }));
+    console.log(formData);
+  };
+
+  const handleLoginSubmit = async (event) => {
+    event.preventDefault();
+
+    const url = 'http://localhost:8000/authenticate';
+    console.log(formData.email);
+    console.log(formData.password);
+    console.log(url);
+
+    axios.post(url, JSON.stringify(formData)).then((response) => {
+      // Writing user id to local storeage
+      //TODO Not secure; must be updated
+      localStorage.setItem('uid', response.data.uid);
+
+      // Ensuring we redirect to profile page
+      setLoggedIn("TRUE");
+    });
+  }
+
+  // Redirecting to profile if user just logged in
+  if (loggedIn == "TRUE") {
+    return <Navigate to='/profile' />;
+}
 
   return (
     <div className="left-side-8-column">
@@ -32,7 +69,7 @@ const LeftSide8Column = () => {
             <span>{` `}</span>
           </span>
         </div>
-        <form className="input-label">
+        <form className="input-label" onSubmit={handleLoginSubmit} method="POST">
           <div className="today-is-a-container">
             <span>
               <p className="today-is-a">{`Today is a new day. It's your day. You shape it. `}</p>
@@ -41,12 +78,25 @@ const LeftSide8Column = () => {
               </p>
             </span>
           </div>
-          <Input
-            placeholderPlaceholder="stageManagersBook@email.com"
-            propPadding1="var(--padding-base) var(--padding-mid)"
-            propWidth="500px"
-            type="email"
-          />
+          <div className="input3">
+            <div className="label1">Email Address</div>
+            <div className="input4">
+              <div className="input5" />
+              <input
+                className="placeholder1"
+                // placeholderPlaceholder="stageManagersBook@email.com"
+                placeholder="stageManagersBook@email.com"
+                // propPadding1="var(--padding-base) var(--padding-mid)"
+                // propWidth="500px"
+                type="email"
+                name="email"
+                id="email"
+                value={formData.email}
+                onChange={handleLoginChange}
+                style={{ width: "500px" }}
+              />
+            </div>
+          </div>
           <div className="input3">
             <div className="label1">Password</div>
             <div className="input4">
@@ -55,10 +105,10 @@ const LeftSide8Column = () => {
                 className="placeholder1"
                 placeholder="Enter Password"
                 type="password"
-                value={placeholderText1Value}
-                onChange={(event) =>
-                  setPlaceholderText1Value(event.target.value)
-                }
+                name="password"
+                id="password"
+                value={formData.password}
+                onChange={handleLoginChange}
                 style={{ width: "500px" }}
               />
             </div>

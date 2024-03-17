bc41944cd9daf22311cacea5abc701751b4c839f
hbrousseau <brousseau.hannah8@gmail.com>
2024-03-16 21:13:48 -0500

    Create Account CSS should finally be okay

--- client/src/pages/sign-up.js
+++ client/src/pages/sign-up.js
@@ -1,26 +1,12 @@
-import { useCallback } from "react";
-import axios, { formToJSON } from 'axios';
-import { GoogleLogin } from "react-google-login";
 import InputFrame from "../components/login-components/sign-up-input-frame";
 import "./sign-up.css";
 import "../global.css";
 
 const SignUp = () => {
-  const onMainButtonContainerClick = useCallback(() => {
-    // Please sync "Profile" to the project
-  }, []);
-
   return (
     <div className="desktop-sign-up">
-      <div className="main-button" onClick={onMainButtonContainerClick}>
-        <div className="sign-in">Sign up</div>
-      </div>
-      <div className="input-frame">
-        <InputFrame />
-      </div>
-      <div className="art1">
-        <img className="login-art1" alt="" src="/create-account-art.png" />
-      </div>
+      <InputFrame />
+      <img className="login-art1" alt="" src="/create-account-art.png" />
     </div>
   );
 };

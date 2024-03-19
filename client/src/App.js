7910a76b7684cdfe459de2fa9c90d77e91b0197f
U-BEN-LAPTOP\Ben10 <bmfioresi@crimson.ua.edu>
2024-03-19 14:42:10 -0500

    cleaned up unit tests for now

--- client/src/App.js
+++ client/src/App.js
@@ -26,6 +26,7 @@ import ProfileForm from './pages/profile-form.js';
 import ProfilePageDemo from './pages/profile-page-demo.js';
 import Script from './pages/scripts.js';
 import ProfileEdit from './pages/profile-edit.js';
+import UnitTests from './pages/unit-tests.js';
 
 function App() {
   return (
@@ -51,6 +52,7 @@ function App() {
           <Route path="/forgotpassword" element={<ForgotPassword />} />
           <Route path="/script" element={<Script />} />
           <Route path="/:hub" element={<HubIndividual />} />
+          <Route path="/unit-tests" element={<UnitTests />} />
         </Routes>
       </Router>
     </div>
